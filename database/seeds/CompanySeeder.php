9369ccfe091110820d178be4ffad8abfdbe87fc5
spgo <spgowrisankar@gmail.com>
2021-02-02 20:52:34 +0530

    new column added

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -12,8 +12,21 @@ class CompanySeeder extends Seeder
     public function run()
     {
         DB::table('companies')->insert([
-            'comp_name' => 'vital',
-            'city' => 'portsmouth'
+            [
+                'comp_name' => 'mandy',
+                'city' => 'Coimbatore',
+                'emp_id' => 2
+            ],
+            [
+                'comp_name' => 'Vital',
+                'city' => 'Portmourth',
+                'emp_id' => 3
+            ],
+            [
+                'comp_name' => 'TCS',
+                'city' => 'Chennai',
+                'emp_id' => 1
+            ]
         ]);
     }
 }
